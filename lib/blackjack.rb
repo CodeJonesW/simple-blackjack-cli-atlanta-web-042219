e458321b11e00d43af2452a653462cf3aed8b991
Will Jones <willjones2010@gmail.com>
2019-03-17 18:32:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card = card1
-  deal_card = card2
+  deal_card = card_one
+  deal_card = card_two
   display_card_total(card_total)
 end
 end
